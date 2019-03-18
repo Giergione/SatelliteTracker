595f1240382e201fc3d13e7d296a1173479c40b1
kahobe <kahobe@ttu.ee>
2019-03-18 13:02:59 +0200

    Refactoring

--- app/src/main/java/com/example/satellitetracker/SatelliteTrackerApplication.java
+++ app/src/main/java/com/example/satellitetracker/SatelliteTrackerApplication.java
@@ -1,7 +1,9 @@
 package com.example.satellitetracker;
 
+import android.Manifest;
 import android.content.Context;
 import android.content.pm.ActivityInfo;
+import android.content.pm.PackageManager;
 import android.graphics.PixelFormat;
 import android.hardware.Camera;
 import android.hardware.Sensor;
@@ -11,43 +13,42 @@ import android.hardware.SensorManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
-import android.view.Window;
-import android.view.animation.Animation;
-import android.view.animation.RotateAnimation;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import java.io.IOException;
 
 
 public class SatelliteTrackerApplication extends AppCompatActivity
-        implements SurfaceHolder.Callback, LocationListener, SensorEventListener { //implements SensorEventListener
+        implements SurfaceHolder.Callback, LocationListener, SensorEventListener {
 
+    //Layout
     private RelativeLayout overlay;
     ImageView yellowDot;
 
+    //Camera utility
+    private static final String TAG = "TrackerApplication";
     private Camera mCamera;
     private SurfaceHolder mSurfaceHolder;
     private boolean isCameraviewOn = false;
 
+    //Location utility
+    private LocationManager locationManager;
+    private String locationProvider;
     TextView LongNr;
     TextView LatNr;
 
+    //Sensor utility
     private static SensorManager sensorManager;
-
-    private LocationManager locationManager;
-    private String locationProvider;
-
-    private SensorManager mSensorManager;
-
-    private Sensor gyrosensor;
-    private Sensor sensorRotation;
+    private Sensor gyroSensor;
+    private Sensor rotationSensor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -58,20 +59,11 @@ public class SatelliteTrackerApplication extends AppCompatActivity
         overlay = findViewById(R.id.overlay);
         yellowDot=findViewById(R.id.marker);
 
+        sensorManager =(SensorManager)getSystemService(Context.SENSOR_SERVICE);
+        gyroSensor = sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
+        rotationSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR);
 
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_camera);
-
-        sensorManager=(SensorManager)getSystemService(Context.SENSOR_SERVICE);
-        sensor = sensorManager.getDefaultSensor(Sensor.TYPE_ORIENTATION);
-        gyrosensor = sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
-
-        setupListeners();
         setupLayout();
-    }
-
-    private void setupListeners() {
-
     }
 
     private void setupLayout() {
@@ -80,12 +72,20 @@ public class SatelliteTrackerApplication extends AppCompatActivity
         mSurfaceHolder = surfaceView.getHolder();
         mSurfaceHolder.addCallback(this);
         mSurfaceHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+
         LongNr = (TextView) findViewById(R.id.LongNr);
+        LatNr = (TextView) findViewById(R.id.LatNr);
     }
 
     @Override
-    public void surfaceChanged(SurfaceHolder holder, int format, int width,
-                               int height) {
+    public void onLocationChanged(Location location) {
+        Log.d(TAG, "onLocationChanged");
+        LongNr.setText(Double.toString(location.getLongitude()));
+        LatNr.setText(Double.toString(location.getLatitude()));
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
         if (isCameraviewOn) {
             mCamera.stopPreview();
             isCameraviewOn = false;
@@ -116,20 +116,64 @@ public class SatelliteTrackerApplication extends AppCompatActivity
         isCameraviewOn = false;
     }
 
-
     @Override
     protected void onResume() {
         super.onResume();
-        if (sensor!=null) {
-            sensorManager.registerListener(this,sensor,SensorManager.SENSOR_DELAY_FASTEST);
-        } else {
-            Toast.makeText(SatelliteTrackerApplication.this, "Sensor not supported", Toast.LENGTH_SHORT).show();
+        Log.d(TAG, "onResume");
+
+        if (locationManager != null) {
+            if (ActivityCompat.checkSelfPermission(this,
+                    Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                    && ActivityCompat.checkSelfPermission(this,
+                    Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                return;
+            }
+            locationManager.requestLocationUpdates(locationProvider, 1000, 1, this);
         }
+
+        if (gyroSensor != null){
+            sensorManager.registerListener(this, gyroSensor, SensorManager.SENSOR_DELAY_NORMAL);
+        }
+        if (rotationSensor != null) {
+            sensorManager.registerListener(this, rotationSensor, SensorManager.SENSOR_DELAY_NORMAL);
+        }
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+        Log.d(TAG, "onStatusChanged " + provider);
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+        Log.d(TAG, "onProviderEnabled " + provider);
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+        Log.d(TAG, "onProviderDisabled " + provider);
     }
 
 
     @Override
+    public void onSensorChanged(SensorEvent event) {
+        Log.d(TAG, "onSensorChanged " + event.sensor.getName() + " x:"
+                + Double.toString( event.values[0]) + " y:"
+                + Double.toString( event.values[1]) + " z:"
+                + Double.toString( event.values[2]));
+
+    }
+
+    @Override
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        Log.d(TAG, "onAccuracyChanged "  + sensor.getName());
 
     }
 
